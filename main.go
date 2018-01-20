8796aecbd753b346caffdf9759f0b424552ccc08
Zate <zate75@gmail.com>
2018-01-20 15:10:02 -0600

    broken, need to swap to bulk get and lookup of user_ids

--- main.go
+++ main.go
@@ -65,6 +65,7 @@ func b2i(b bool) int {
 }
 
 func last200() {
+
 	var k apikeys
 	var consumerkey string
 	var accesskey string
@@ -82,50 +83,55 @@ func last200() {
 	token := oauth1.NewToken(accesskey, accesssecret)
 	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
-
+	if FollowerCount > 200 {
+		FollowerCount = 200
+	}
 	var cursor int64
 	cursor = -1
-
+	var allfollowers []twitter.User
 	for cursor != 0 {
-		var allfollowers []twitter.User
-		followers, _, err := client.Followers.List(&twitter.FollowerListParams{Cursor: cursor, Count: 100})
+
+		fParams := &twitter.FollowerListParams{}
+		fParams.Cursor = cursor
+		fParams.Count = FollowerCount
+		fParams.ScreenName = ScreenName
+		followers, _, err := client.Followers.List(fParams)
 		CheckErr(err)
-		//log.Println(resp)
+		log.Println(followers)
 		//cursor = 0 // if cursor is 0, then it will only run through 1 lot of followers from twitter of the size you specify above (default 200)
 		cursor = followers.NextCursor // If you comment out cursor = 0 and uncomment this, it will iterate through ALL followers in batches as sized above (default 200)
+		log.Println(ScreenName)
 		for k := range followers.Users {
+
 			allfollowers = append(allfollowers, followers.Users[k])
-			// 1 	1:0.7 3:0.1 9:0.4
-			// 0	2:0.3 4:0.9 7:0.5
-			// 0	2:0.7 5:0.3
-			// ... This is the format for libsvm for hector.
-		}
-		if GetTrainingData == false {
-			do200(allfollowers, client)
-		} else {
-			log200(allfollowers, client)
 		}
 
 		// Need this to not hit twitter rate limits.  If you are using NextCursor above, them have this uncommented also so that you will only make one request for a batch of followers per minute.  This will be right on with the API rate limit of 15 in 15 mins.
 		//log.Println(cursor)
-		if cursor != 0 {
+		if cursor != 0 && FollowerCount > 199 {
+			log.Println("got here")
 			time.Sleep(time.Duration(60) * time.Second)
 		}
+	}
+	if GetTrainingData == false {
+		do200(allfollowers, client)
+	} else {
+		log200(allfollowers, client)
 	}
 }
 
 func do200(allfollowers []twitter.User, client *twitter.Client) {
-
+	log.Println("do200")
 	var fc []int
 	var listed []int
 	var tweets []int
 	var favc []int
 
-	f, err := os.OpenFile("followers.t", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	CheckErr(err)
-	defer f.Close()
+	// f, err := os.OpenFile("followers.t", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	// CheckErr(err)
+	// defer f.Close()
 
-	w := bufio.NewWriter(f)
+	// w := bufio.NewWriter(f)
 
 	for x := range allfollowers {
 		fc = append(fc, allfollowers[x].FollowersCount)
@@ -164,15 +170,15 @@ func do200(allfollowers []twitter.User, client *twitter.Client) {
 			idxprofileimage,
 			idxfollowing,
 			idxverified)
-		fmt.Fprintf(w, "0	1:%v 2:%v 3:%v 4:%v 5:%v 6:%v 7:%v 8:%v\n",
-			idxfavs,
-			idxfollowers,
-			idxlisted,
-			idxtweets,
-			idxprofile,
-			idxprofileimage,
-			idxfollowing,
-			idxverified)
+		// fmt.Fprintf(w, "0	1:%v 2:%v 3:%v 4:%v 5:%v 6:%v 7:%v 8:%v\n",
+		// 	idxfavs,
+		// 	idxfollowers,
+		// 	idxlisted,
+		// 	idxtweets,
+		// 	idxprofile,
+		// 	idxprofileimage,
+		// 	idxfollowing,
+		// 	idxverified)
 		_, _, _, method, params := doParams()
 		model, _ := params["model"]
 		c := &Classifier{
@@ -183,20 +189,22 @@ func do200(allfollowers []twitter.User, client *twitter.Client) {
 		res := c.testFollower(fdata)
 		if res > 0.02 && allfollowers[x].Verified != true && allfollowers[x].Following != true {
 			log.Printf("%v is a bot : %v", allfollowers[x].ScreenName, res)
-			user, resp, _ := client.Block.Create(&twitter.BlockUserParams{ScreenName: allfollowers[x].ScreenName})
-			//log.Println(resp)
-			if resp.StatusCode == 200 {
-				log.Printf("%v was blocked", user.ScreenName)
-			}
-			user, resp, _ = client.Block.Destroy(&twitter.BlockUserParams{ScreenName: allfollowers[x].ScreenName})
-			if resp.StatusCode == 200 {
-				log.Printf("%v was unblocked", user.ScreenName)
+			if DeFang == false {
+				user, resp, _ := client.Block.Create(&twitter.BlockUserParams{ScreenName: allfollowers[x].ScreenName})
+				//log.Println(resp)
+				if resp.StatusCode == 200 {
+					log.Printf("%v was blocked", user.ScreenName)
+				}
+				user, resp, _ = client.Block.Destroy(&twitter.BlockUserParams{ScreenName: allfollowers[x].ScreenName})
+				if resp.StatusCode == 200 {
+					log.Printf("%v was unblocked", user.ScreenName)
+				}
 			}
 		} else {
 			log.Printf("%v not bot : %v Verified: %v Following: %v", allfollowers[x].ScreenName, res, allfollowers[x].Verified, allfollowers[x].Following)
 		}
 	}
-	w.Flush()
+	// w.Flush()
 	log.Println("Run done")
 	// 1 - ScreenName - unique - dont put these in training
 	// 2 - user id - unique -  dont put these in training
@@ -213,7 +221,7 @@ func do200(allfollowers []twitter.User, client *twitter.Client) {
 }
 
 func log200(allfollowers []twitter.User, client *twitter.Client) {
-
+	log.Println("log200")
 	var fc []int
 	var listed []int
 	var tweets []int
@@ -221,7 +229,7 @@ func log200(allfollowers []twitter.User, client *twitter.Client) {
 	var langc []int
 	var desc []int
 
-	f, err := os.OpenFile("followers.t", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	f, err := os.OpenFile("followers_new_training.t", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	CheckErr(err)
 	defer f.Close()
 
@@ -266,7 +274,7 @@ func log200(allfollowers []twitter.User, client *twitter.Client) {
 		lang := new(big.Int).SetBytes([]byte(allfollowers[x].Lang)).Uint64()
 		idxlang := math.Log10(float64(lang)) / math.Log10(float64(langcMax))
 		des := new(big.Int).SetBytes([]byte(allfollowers[x].Description)).Uint64()
-		log.Println(des)
+		//log.Println(des)
 		idxdesc := math.Log10(float64(des+1)) / math.Log10(float64(descMax+1))
 
 		// fdata := fmt.Sprintf("0	1:%v 2:%v 3:%v 4:%v 5:%v 6:%v 7:%v 8:%v",
@@ -289,7 +297,7 @@ func log200(allfollowers []twitter.User, client *twitter.Client) {
 		// 	idxfollowing,
 		// 	idxverified,
 		// 	idxlang)
-		fmt.Printf("0	ScreenName:%v 1:%v 2:%v 3:%v 4:%v 5:%v 6:%v 7:%v 8:%v 9:%v 10:%v\n",
+		fmt.Fprintf(w, "0	ScreenName:%v 1:%v 2:%v 3:%v 4:%v 5:%v 6:%v 7:%v 8:%v 9:%v 10:%v\n",
 			allfollowers[x].ScreenName,
 			idxfavs,
 			idxfollowers,
@@ -451,8 +459,20 @@ func doParams() (string, string, string, string, map[string]string) {
 // GetTrainingData is a global command line flag to swap to just gathering training data.
 var GetTrainingData bool
 
+// DeFang stops the program from taking action and just reports on the users.
+var DeFang bool
+
+// FollowerCount is the number of followers to return and classify.
+var FollowerCount int
+
+// ScreenName is the ScreenName of the user to classify.
+var ScreenName string
+
 func init() {
-	flag.BoolVar(&GetTrainingData, "gtd", true, "Dump followers to a file to be used to generate new training data.")
+	flag.BoolVar(&GetTrainingData, "gtd", false, "Dump followers to a file to be used to generate new training data.")
+	flag.BoolVar(&DeFang, "df", true, "Process the followers list but do not block/unblock or take and action.")
+	flag.IntVar(&FollowerCount, "fc", 1, "Number of followers to return and classify.")
+	flag.StringVar(&ScreenName, "sn", "Zate", "ScreenName of the user to classify.")
 	flag.Parse()
 }
 
@@ -460,13 +480,13 @@ func main() {
 
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 	log.Println("Botceptor Coming Online ....")
-	if GetTrainingData == false {
-		for {
-			last200()
-			log.Println("Pausing for 60 seconds.")
-			time.Sleep(time.Duration(60) * time.Second)
-		}
-	} else {
-		last200()
-	}
+	// if GetTrainingData == false {
+	// 	for {
+	// 		last200()
+	// 		log.Println("Pausing for 60 seconds.")
+	// 		time.Sleep(time.Duration(60) * time.Second)
+	// 	}
+	// } else {
+	last200()
+	// }
 }
